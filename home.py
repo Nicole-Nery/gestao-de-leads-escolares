2623836c9ccc410cd504202e7c3f1ce02e632441
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-23 14:56:31 -0300

    Update home.py

--- home.py
+++ home.py
@@ -2,4 +2,9 @@ import streamlit as st
 from db import supabase
 
 def show_home():
+    # Estilo CSS
+    caminho_css = "style/main.css"
+    with open(caminho_css) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
     st.write("Oie")
