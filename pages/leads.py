a2ca9e9eacb82c69f58ee7ba6aea0ca4a938c4d7
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 14:27:37 -0300

    Create leads.py

---
+++ pages/leads.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Leads", layout="wide")
+st.title("📋 Gerenciamento de Leads")
+st.info("Aqui você poderá cadastrar, visualizar, editar e excluir leads.")
