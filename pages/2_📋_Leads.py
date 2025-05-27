f7670bc29650808cd8279c14f184c99a304b03c2
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 17:00:10 -0300

    Update 2_📋_Leads.py

--- pages/2_📋_Leads.py
+++ pages/2_📋_Leads.py
@@ -2,6 +2,10 @@ import streamlit as st
 
 st.set_page_config(page_title="Leads", layout="wide")
 
+# Estilo CSS
+with open("style/main.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    
 if "usuario" not in st.session_state:
     st.error("Você precisa estar logado para acessar esta página.")
     st.stop()
