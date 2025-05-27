7d3967a6017cd9305da71597e6b9aca1c3bafa15
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 15:13:51 -0300

    Update and rename leads.py to 2_📋_Leads.py

--- pages/leads.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def pagina_leads():
-    st.set_page_config(page_title="Leads", layout="wide")
-    st.title("📋 Gerenciamento de Leads")
-    st.info("Aqui você poderá cadastrar, visualizar, editar e excluir leads.")
