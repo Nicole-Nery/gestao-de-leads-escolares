799a06d473bd3866cf0ee1d62fc5afccd0fc900c
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 16:48:58 -0300

    Update login_page.py

--- login_page.py
+++ login_page.py
@@ -2,18 +2,6 @@ import streamlit as st
 from auth.funcoes_auth import *
 
 def mostrar_tela_login_ou_cadastro():
-    st.markdown("""
-        <style>
-        [data-testid="stSidebarNav"] [data-testid="stSidebarNavLink"] span::after {
-            content: "Gestão de Leads";
-            visibility: visible;
-            position: absolute;
-            left: 0;
-        }
-        </style>
-        """, unsafe_allow_html=True)
-
-    
     if "modo" not in st.session_state or st.session_state["modo"] not in ["login", "cadastro"]:
         st.session_state["modo"] = "login"
 
