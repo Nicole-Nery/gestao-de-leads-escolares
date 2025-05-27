45e406a5a852724aea69f73a229d90cd7e154cc4
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 16:26:24 -0300

    Update 3_💬_Interações.py

--- pages/3_💬_Interações.py
+++ pages/3_💬_Interações.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 if "usuario" not in st.session_state:
-    st.switch_page("pages/0_🔐_Login.py")
+    st.error("Você precisa estar logado para acessar esta página.")
+    st.stop()
 
 st.title("Interações")
 
