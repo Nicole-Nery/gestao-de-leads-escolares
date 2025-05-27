5e95442b0dad1f458ff7e3793514df34aeb811cc
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 15:56:50 -0300

    Update 2_📋_Leads.py

--- pages/2_📋_Leads.py
+++ pages/2_📋_Leads.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Leads", layout="wide")
 
 if "usuario" not in st.session_state:
-    st.switch_page("pages/_0_🔐_Login.py")
+    st.switch_page("pages/0_🔐_Login.py")
 
 st.title("📋 Gerenciamento de Leads")
 st.info("Aqui você poderá cadastrar, visualizar, editar e excluir leads.")
