af1dd44ba342366be3f00fc5cfaede58a2d2d394
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 17:00:19 -0300

    Update 3_💬_Interações.py

--- pages/3_💬_Interações.py
+++ pages/3_💬_Interações.py
@@ -4,5 +4,9 @@ if "usuario" not in st.session_state:
     st.error("Você precisa estar logado para acessar esta página.")
     st.stop()
 
+# Estilo CSS
+with open("style/main.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 st.title("Interações")
 
