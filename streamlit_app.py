9acf9129dfad0b6079c1d411d5f68c1eefa96317
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-27 15:37:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from auth.funcoes_login import *
+from auth.funcoes_auth import *
+from login_page import *
 
 st.set_page_config(page_title= "Gestão de Leads", layout = "wide")
 
@@ -7,42 +8,10 @@ st.set_page_config(page_title= "Gestão de Leads", layout = "wide")
 with open("style/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# Modos
-modos_validos = ["login", "cadastro", "home", "leads"]
-
-if "modo" not in st.session_state or st.session_state["modo"] not in modos_validos:
-    st.session_state["modo"] = "login"
-
-modo = st.session_state.get("modo", "login")
-
-if modo in ["login"]:
-    st.markdown("""
-        <style>
-            .block-container {
-                padding-top: 5vh;
-                max-width: 500px;
-                margin: auto;
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
-if modo in ["cadastro"]:
-    st.markdown("""
-        <style>
-            .block-container {
-                padding-top: 5vh;
-                max-width: 900px;
-                margin: auto;
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
 # Fluxo - Login ou cadastro
 if "usuario" not in st.session_state:
-    if st.session_state["modo"] == "login":
-        login()
-    else:
-        cadastro()
+    mostrar_tela_login_ou_cadastro()
     st.stop()
 
+# Se já está autenticado, direciona para home
 st.switch_page("pages/1_🏠_Home.py") 
