e77ddf4dc1407a4ffb26a3d0a2c1bf41541db41d
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 12:25:31 -0300

    Update 1_🏠_Home.py

--- pages/1_🏠_Home.py
+++ pages/1_🏠_Home.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from db import supabase
 
+st.markdown("""
+        <style>
+        /* Oculta o texto da página ativa na sidebar */
+        [data-testid="stSidebarNav"] ul li:first-child a span {
+            display: none;
+        }
+        </style>
+    """, unsafe_allow_html=True)
+
 if "usuario" not in st.session_state:
     st.error("Você precisa estar logado para acessar esta página.")
     st.stop()
