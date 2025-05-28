7689033df0af0d8b7571c9dfeb9ff18924d1793c
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 18:43:20 -0300

    Update 2_📋_Leads.py

--- pages/2_📋_Leads.py
+++ pages/2_📋_Leads.py
@@ -4,4 +4,24 @@ from cabecalho import *
 
 conexao_e_cabecalho()
 
-st.title("Gerenciamento de Leads")
+st.subheader("Gerenciamento de Leads")
+
+tabs_leads = st.tabs(["Cadastrar", "Visualizar", "Atualizar", "Excluir"])
+
+with tabs_leads[0]:
+    with st.form("form_cadastro_leads"):
+        st.subheader("👨‍👩‍👧 Informações dos Responsáveis")
+        nome_responsavel = st.text_input("Nome do responsável")
+        telefone = st.text_input("Telefone de contato")
+        email = st.text_input("E-mail")
+
+        st.markdown("---")
+
+
+        st.subheader("📌 Informações do Aluno")
+        nome = st.text_input("Nome do aluno")
+        idade = st.number_input("Idade", min_value=0)
+        escola_origem = st.text_input("Nome do aluno")
+
+
+
