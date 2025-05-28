f4afcbf8a20b9f7535f3f17f98b6affb1e313c2d
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 16:15:01 -0300

    Update 3_💬_Interações.py

--- pages/3_💬_Interações.py
+++ pages/3_💬_Interações.py
@@ -1,13 +1,8 @@
 import streamlit as st
 from db import supabase
+from cabecalho import *
 
-if "usuario" not in st.session_state:
-    st.error("Você precisa estar logado para acessar esta página.")
-    st.stop()
-
-# Estilo CSS
-with open("style/main.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+conexao_e_cabecalho()
 
 st.title("Interações")
 
