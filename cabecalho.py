e130e3d90c6150a3ea612b8ba3630a2c181507ff
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 16:56:26 -0300

    Update cabecalho.py

--- cabecalho.py
+++ cabecalho.py
@@ -23,13 +23,15 @@ def conexao_e_cabecalho():
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     with st.container():
-        st.markdown('<div class="fixed-header">', unsafe_allow_html=True)
+        '''st.markdown('<div class="fixed-header">', unsafe_allow_html=True)
         col1, col2 = st.columns([1,5])
         with col1:
             st.image(str(logo_path), width=300)
         with col2:
             st.html("<div class='header-title'>Funil Inteligente de Leads</div>")
-        st.markdown('</div>', unsafe_allow_html=True)
+        st.markdown('</div>', unsafe_allow_html=True)'''
+
+        st.title("Funil Inteligente de Leads Escolares")
         st.markdown(f"Bem-vindo, **{usuario['nome_usuario']}** ({usuario['cargo']})!")
 
     st.markdown("---")
