ef17249ac8ed2e2a5059513b7111f2216c30ecd2
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 16:14:49 -0300

    Update 2_📋_Leads.py

--- pages/2_📋_Leads.py
+++ pages/2_📋_Leads.py
@@ -1,13 +1,8 @@
 import streamlit as st
 from db import supabase
+from cabecalho import *
 
-# Estilo CSS
-with open("style/main.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    
-if "usuario" not in st.session_state:
-    st.error("Você precisa estar logado para acessar esta página.")
-    st.stop()
+conexao_e_cabecalho()
 
 st.title("📋 Gerenciamento de Leads")
 st.info("Aqui você poderá cadastrar, visualizar, editar e excluir leads.")
