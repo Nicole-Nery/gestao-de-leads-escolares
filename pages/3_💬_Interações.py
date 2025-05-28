62cc9a555c3c78ab0de4aa668067c46dacf6143f
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 13:14:15 -0300

    Update 3_💬_Interações.py

--- pages/3_💬_Interações.py
+++ pages/3_💬_Interações.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from db import supabase
 
-st.markdown("""
+'''
+    st.markdown("""
         <style>
         /* Oculta o texto da página ativa na sidebar */
         [data-testid="stSidebarNav"] ul li:first-child a span {
@@ -9,6 +10,7 @@ st.markdown("""
         }
         </style>
     """, unsafe_allow_html=True)
+    '''
 
 if "usuario" not in st.session_state:
     st.error("Você precisa estar logado para acessar esta página.")
