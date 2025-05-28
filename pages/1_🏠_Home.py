02df714145e3367d2b871001d75e9c261c1dcb49
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 17:11:05 -0300

    Update 1_🏠_Home.py

--- pages/1_🏠_Home.py
+++ pages/1_🏠_Home.py
@@ -4,13 +4,12 @@ from cabecalho import *
 
 conexao_e_cabecalho()
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns([5,1])
 
 with col1:
-    st.link_button("📋 Cadastrar Lead", url="/pages/2_📋_Leads")
+    st.write("Olá")
 
 with col2:
+    st.link_button("📋 Cadastrar Lead", url="/pages/2_📋_Leads")
     st.link_button("💬 Histórico de Interações", url="/pages/3_💬_Interações")
-
-with col3:
     st.link_button("📊 Painel de KPIs", url="/pages/4_📊_KPIs")
