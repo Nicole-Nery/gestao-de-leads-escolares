ca95de9e32e37fdd14d186a8ff73ed81342b45f0
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 16:14:38 -0300

    Update 1_🏠_Home.py

--- pages/1_🏠_Home.py
+++ pages/1_🏠_Home.py
@@ -1,27 +1,8 @@
 import streamlit as st
 from db import supabase
+from cabecalho import *
 
-if "usuario" not in st.session_state:
-    st.error("Você precisa estar logado para acessar esta página.")
-    st.stop()
-
-usuario = st.session_state["usuario"]
-
-# Estilo CSS
-with open("style/main.css") as f:
-    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-with st.container():
-    st.markdown('<div class="fixed-header">', unsafe_allow_html=True)
-    col1, col2 = st.columns([1,5])
-    with col1:
-        st.image('assets/logo-sigah.svg', width=300)
-    with col2:
-        st.html("<div class='header-title'>Sistema Integrado de Gestão de Atas Hospitalares</div>")
-    st.markdown('</div>', unsafe_allow_html=True)
-    st.markdown(f"Bem-vindo, **{usuario['nome_usuario']}** ({usuario['cargo']})!")
-
-st.markdown("---")
+conexao_e_cabecalho()
 
 col1, col2, col3 = st.columns(3)
 
