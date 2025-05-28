4d100903f11b8281341b0e0373dcd8a95e9abb55
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 13:19:26 -0300

    Update 0_🔐_Login.py

--- 0_🔐_Login.py
+++ 0_🔐_Login.py
@@ -4,15 +4,6 @@ from login_page import *
 
 st.set_page_config(page_title= "Gestão de Leads", layout = "wide")
 
-st.markdown("""
-        <style>
-        /* Oculta o texto da página ativa na sidebar */
-        [data-testid="stSidebarNav"] ul li:first-child a span {
-            display: none;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-
 # Estilo CSS
 with open("style/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -23,4 +14,3 @@ if "usuario" not in st.session_state:
     st.stop()
 
 st.switch_page("pages/1_🏠_Home.py")
-
