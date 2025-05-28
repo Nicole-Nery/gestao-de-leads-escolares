d2724fc853d01503a8829d9c8406c8f5f1d07def
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 16:03:15 -0300

    Update 1_🏠_Home.py

--- pages/1_🏠_Home.py
+++ pages/1_🏠_Home.py
@@ -11,8 +11,15 @@ usuario = st.session_state["usuario"]
 with open("style/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-st.title("🏠 Painel Inicial")
-st.markdown(f"Bem-vindo, **{usuario['nome_usuario']}** ({usuario['cargo']})!")
+with st.container():
+    st.markdown('<div class="fixed-header">', unsafe_allow_html=True)
+    col1, col2 = st.columns([1,5])
+    with col1:
+        st.image('assets/logo-sigah.svg', width=300)
+    with col2:
+        st.html("<div class='header-title'>Sistema Integrado de Gestão de Atas Hospitalares</div>")
+    st.markdown('</div>', unsafe_allow_html=True)
+    st.markdown(f"Bem-vindo, **{usuario['nome_usuario']}** ({usuario['cargo']})!")
 
 st.markdown("---")
 
