d25cfd35d6ec504af4bcf68f0f3fb87e542c80a2
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 13:20:17 -0300

    Update 2_📋_Leads.py

--- pages/2_📋_Leads.py
+++ pages/2_📋_Leads.py
@@ -1,17 +1,6 @@
 import streamlit as st
 from db import supabase
 
-'''
-    st.markdown("""
-        <style>
-        /* Oculta o texto da página ativa na sidebar */
-        [data-testid="stSidebarNav"] ul li:first-child a span {
-            display: none;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-    '''
-
 # Estilo CSS
 with open("style/main.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
