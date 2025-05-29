0daea77127867f2dfb2864370cebac02c12ab477
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-29 13:24:44 -0300

    Update cabecalho.py

--- cabecalho.py
+++ cabecalho.py
@@ -36,11 +36,8 @@ def conexao_e_cabecalho():
         st.markdown(f"Bem-vindo, **{usuario['nome_usuario']}** ({usuario['cargo']})!")'''
     
     st.html("<div class='header-title'>Funil Inteligente de Leads Escolares</div>")
-    st.markdown(
-            f"""<p style='margin-bottom:0;'><strong>Escola Colibri</strong></p>
-            <p style='margin-top:0;'>Olá, {usuario['nome_usuario']} - {usuario['cargo']}</p>""",
-            unsafe_allow_html=True
-        )
-
+    
+    st.markdown(f"""<p style='margin-bottom:0;'><strong>Escola Colibri</strong></p>""", unsafe_allow_html=True)
+    st.markdown(f"Olá, **{usuario['nome_usuario']}** ({usuario['cargo']})!")
 
     st.markdown("---")
