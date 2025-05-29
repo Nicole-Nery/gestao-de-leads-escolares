35cf8380beb8490c351e4512f0b25537f7788e01
Nicole Nery <155188172+Nicole-Nery@users.noreply.github.com>
2025-05-28 22:01:03 -0300

    Update 1_🏠_Home.py

--- pages/1_🏠_Home.py
+++ pages/1_🏠_Home.py
@@ -10,5 +10,4 @@ with col1:
     st.write("Olá")
 
 with col2:
-    #st.link_button("Cadastrar Lead", url="/pages/2_📋_Leads", new_tab=False)
     st.page_link("pages/2_📋_Leads.py", label="Cadastrar Lead", icon="➕", new_tab=False)
